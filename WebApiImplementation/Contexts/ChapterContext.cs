b9ca81863ba42cd0199c792a1598f6e20ff432b3
neicruz <nei.cruz@hotmail.com>
2022-03-24 14:28:34 -0300

    ApiWeb Complete  - Create, Search, Update and Delete methods

---
+++ WebApiImplementation/Contexts/ChapterContext.cs
@@ -0,0 +1,25 @@
+﻿using WebApiImplementation.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiImplementation.Contexts
+{
+    public class ChapterContext:DbContext
+    {
+        public ChapterContext()
+        {
+
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Password=1234567;Persist Security Info=True;User ID=sa;Initial Catalog=Chapter;Data Source=DESKTOP-AJNAII3\\SQLEXPRESS");
+            }
+        }
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
---
+++ WebApiImplementation/Controllers/LivrosController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiImplementation.Models;
+using WebApiImplementation.Repositories;
+
+namespace WebApiImplementation.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivrosController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(_livroRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro livro)
+        {
+            _livroRepository.Cadastrar(livro);
+            return StatusCode(201);
+        }
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorID(int id)
+        {
+            Livro livro = _livroRepository.BuscarPorId(id);
+
+            if (livro ==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(livro);
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Livro livro)
+        {
+            _livroRepository.Atualizar(id, livro);
+            return StatusCode(204);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            _livroRepository.Deletar(id);
+            return StatusCode(204);
+        }
+        
+
+    
+
+    }
+
+}
